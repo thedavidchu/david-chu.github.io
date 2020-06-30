261b86c793512d205b836579baa4f014c13d73b9
thedavidchu <thedavidchu@gmail.com>
2020-06-29 21:57:32 -0400

    Toggle title

--- blink_title.js
+++ blink_title.js
@@ -1,5 +1,16 @@
 function blink_title(){
-	var title = "Is this your idea of adventure?";
-	
-	document.title = title;
+	var title_adventure = "Is this your idea of adventure?";
+	var title_main = "David Chu - Main page"	
+
+	switch(document.title){
+		case title_adventure:
+			document.title = title_main;
+			break;
+		case title_main:
+			document.title = title_adventure;
+			break;
+		default:
+			document.title = title_main;
+			break;
+	}
 }
