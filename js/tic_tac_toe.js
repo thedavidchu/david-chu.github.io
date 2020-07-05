a24194a89c7c0c68851b73a74f17fa355066dfeb
thedavidchu <thedavidchu@gmail.com>
2020-07-05 00:28:50 -0400

    Fixed ttt bugs in that declared for wrong player and did not stop play after win.

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -62,6 +62,14 @@ function get_move(player, board){
 }
 
 
+
+
+
+
+
+
+
+
 function toggle_player(player){
 	if(player==1){
 		player = 2;										// Does this change the player outside of the function?
@@ -78,22 +86,34 @@ function toggle_player(player){
 function analyze_board(board){
 	/* Check board conditions. It will return 0 if the game is still in play OR the player number that won OR 3 if the game is a draw */
 	var draw=true;
+	// 012
+	// 345
+	// 678
 	// Check win conditions. (012,345,678; 036,147,258; 048,246)
 	/*  */if(board[0]==board[1] && board[0]==board[2] && board[0]!=0){
+		console.log(0);
 		return board[0];
 	}else if(board[3]==board[4] && board[3]==board[5] && board[3]!=0){
+		console.log(1);
 		return board[3];
 	}else if(board[6]==board[7] && board[6]==board[8] && board[6]!=0){
+		console.log(2);
 		return board[6];
 	}else if(board[0]==board[3] && board[0]==board[6] && board[0]!=0){
+		console.log(3);
 		return board[0];
 	}else if(board[1]==board[4] && board[1]==board[7] && board[1]!=0){
+		console.log(4);
 		return board[1];
 	}else if(board[2]==board[5] && board[2]==board[8] && board[2]!=0){
+		console.log(5);
 		return board[2];
 	}else if(board[0]==board[4] && board[0]==board[8] && board[0]!=0){
+		console.log(6);
 		return board[0];
 	}else if(board[2]==board[4] && board[2]==board[6] && board[2]!=0){
+		console.log(7);
+		console.log(board[2]);
 		return board[2];
 	}
 
@@ -110,6 +130,13 @@ function analyze_board(board){
 	}else{
 		// Game in play
 		return 0;
+	}
+}
+
+
+function end_game(){
+	for(i=0;i<9;i++){
+		document.getElementById("ttt_"+i).disabled = true;
 	}
 }
 
@@ -179,17 +206,25 @@ function get_player(board){
 
 function ttt_main_buttons(pushed_button){
 	var board = get_board();
-	console.log("Board:", board);
+	var state;
 	var player = get_player(board);
-	console.log(player);
 
 	document.getElementById(pushed_button).innerHTML = player;
 	document.getElementById(pushed_button).value = player;
 	document.getElementById(pushed_button).disabled = true;
 
-	if(analyze_board(board) == 1 || analyze_board(board) == 2){
-		document.getElementById("ttt").innerHTML = "Player "+player+" won!";
-	}else if(analyze_board(board) == "3"){
+	board = get_board();
+	state = analyze_board(board);
+
+	if(state == 1 || state == 2){
+		console.log("Win by", state);
+		document.getElementById("ttt").innerHTML = "Player "+state+" won!";
+		end_game();
+
+	}else if(state == "3"){
+		console.log("Cat's game!");
 		document.getElementById("ttt").innerHTML = "Cat's Game!";
+		end_game();
 	}
+	console.log("END TURN", state);
 }
